4cf4f7bffe3b3afe2e811f3d657455d00fad8297
OLEANG <wannachok.job@gmail.com>
2024-10-16 20:56:07 +0700

    feat/hamburgerbutton: make it response,change a picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,40 @@
 import "./App.css";
+import Hamburger from 'hamburger-react'
+import { useState } from "react";
+
+function HamburgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="flex">
+      <button
+        className="text-3xl focus:outline-none md:hidden"
+        onClick={toggleMenu}
+      >
+        <Hamburger />
+      </button>
+      <nav
+        className={`${
+          isOpen ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-10"
+        }transition-all duration-300 ease-in-out absolute top-20 left-1/2 transform -translate-x-1/2 w-full shadow-xl bg-brown-100 p-20 rounded-lg md:flex md:static md:bg-transparent md:w-auto`}
+      >
+        <div className="flex flex-col items-center gap-5">
+          <button className="transition hover:scale-105 hover:shadow-xl bg-white border-[0.01rem] rounded-full border-black px-40 py-2 sm:px-6 font-semibold mb-2">
+            Log in
+          </button>
+          <button className="transition hover:scale-105 hover:shadow-xl bg-black border-[0.01rem] rounded-full px-40 py-2 sm:px-6 font-semibold text-white">
+            Sign up
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
 
 function NavBar() {
   return (
@@ -26,29 +62,16 @@ function NavBar() {
         </svg>
       </div>
       <div className="flex space-x-4 pr-4 sm:pr-20 ">
-        <button className="bg-white border-[0.01rem] rounded-full border-black px-4 sm:px-6 font-semibold hidden sm:block">
+        <button className="transition hover:scale-105 hover:shadow-xl bg-white border-[0.01rem] rounded-full border-black px-4 sm:px-6 font-semibold hidden sm:block">
           Log in
         </button>
-        <button className="bg-black border-[0.01rem] rounded-full px-4 sm:px-6 font-semibold text-white hidden sm:block">
+        <button className="transition hover:scale-105 hover:shadow-xl bg-black border-[0.01rem] rounded-full px-4 sm:px-6 font-semibold text-white hidden sm:block">
           Sign up
         </button>
-        <button className="block sm:hidden">
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M3 6.75C3 6.55109 3.07902 6.36032 3.21967 6.21967C3.36032 6.07902 3.55109 6 3.75 6H20.25C20.4489 6 20.6397 6.07902 20.7803 6.21967C20.921 6.36032 21 6.55109 21 6.75C21 6.94891 20.921 7.13968 20.7803 7.28033C20.6397 7.42098 20.4489 7.5 20.25 7.5H3.75C3.55109 7.5 3.36032 7.42098 3.21967 7.28033C3.07902 7.13968 3 6.94891 3 6.75ZM3 12C3 11.8011 3.07902 11.6103 3.21967 11.4697C3.36032 11.329 3.55109 11.25 3.75 11.25H20.25C20.4489 11.25 20.6397 11.329 20.7803 11.4697C20.921 11.6103 21 11.8011 21 12C21 12.1989 20.921 12.3897 20.7803 12.5303C20.6397 12.671 20.4489 12.75 20.25 12.75H3.75C3.55109 12.75 3.36032 12.671 3.21967 12.5303C3.07902 12.3897 3 12.1989 3 12ZM3 17.25C3 17.0511 3.07902 16.8603 3.21967 16.7197C3.36032 16.579 3.55109 16.5 3.75 16.5H20.25C20.4489 16.5 20.6397 16.579 20.7803 16.7197C20.921 16.8603 21 17.0511 21 17.25C21 17.4489 20.921 17.6397 20.7803 17.7803C20.6397 17.921 20.4489 18 20.25 18H3.75C3.55109 18 3.36032 17.921 3.21967 17.7803C3.07902 17.6397 3 17.4489 3 17.25Z"
-              fill="#75716B"
-            />
-          </svg>
-        </button>
-      </div>
+        <div className=" block sm:hidden">
+          <HamburgerMenu />
+        </div>
+      </div>  
     </nav>
   );
 }
@@ -70,13 +93,13 @@ function HeroSection() {
       <div className="w-full sm:w-[30rem] h-[20rem] sm:h-[40rem] overflow-hidden rounded-lg shadow-2xl">
         <img
           className="w-full h-full object-cover"
-          src="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449784/my-blog-post/xgfy0xnvyemkklcqodkg.jpg"
-          alt="รูปคนหมวกสีน้ำตาล"
+          src="src\assets\img\oleang-img1.jpg"
+          alt="รูปคนเสื้อสีดำ"
         />
       </div>
       <div className="w-full sm:w-[20rem] h-auto flex flex-col p-4 rounded-lg">
         <a className="text-xl">-Author</a>
-        <h2 className="text-3xl font-semibold">Thompson P.</h2>
+        <h2 className="text-3xl font-semibold">Wannachok T.</h2>
         <p className="mt-4">
           I am a pet enthusiast and freelance writer who specializes in animal
           behavior and care. With a deep love for cats, I enjoy sharing insights
